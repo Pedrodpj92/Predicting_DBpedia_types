07b7ea5dd06df7ffb30383b015c6ef8891dd5703
Pedro del Pozo <pedrodpj92@gmail.com>
2018-01-16 15:50:27 +0100

    1st reorganize and regroup code

---
+++ approach1/test1/modela_aproach1_test1.R
@@ -0,0 +1,95 @@
+#!/usr/bin/env Rscript
+#modela_approach1_test1.R
+
+
+library(h2o)
+h2o.init(
+  nthreads=-1            ## -1: use all available threads
+  #max_mem_size = "2G"
+)
+#h2o.removeAll()
+
+
+
+#carga datos escritos desde preparaDatos_SoloHojas.R
+df_training_test1 <- h2o.importFile(path = normalizePath("./data/approach1/test1/training.csv"), header = TRUE)
+df_validating_test1 <- h2o.importFile(path = normalizePath("./data/approach1/test1/validating_test1.csv"), header = TRUE)
+
+train_test1 <- h2o.assign(df_training_test1, "train_test1.hex")
+valid_test1 <- h2o.assign(df_validating_test1, "valid_test1.hex")
+
+############
+#naiveBayes#
+############
+nb_pruebaHojas_test1 <- h2o.naiveBayes(
+  model_id="nb_pruebaHojas_test1",
+  training_frame=train_test1, 
+  validation_frame=valid_test1[,2:ncol(valid_test1)],
+  x=2:(ncol(train_test1)-1),
+  y=ncol(train_test1),
+  seed = 1234)
+h2o.saveModel(nb_pruebaHojas_test1, path="./output/models")
+
+test1 <- h2o.predict(object = nb_pruebaHojas_test1, newdata = valid_test1[,2:(ncol(valid_test1)-1)])[1]
+
+salida_test1 <- cbind(validating_test1[,1],as.data.frame(test1))
+salida_test1$p <- "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>"
+salida_test1[,c(1,2,3)] <- salida_test1[,c(1,3,2)]
+colnames(salida_test1) <- c("s","o","p")
+colnames(salida_test1) <- c("s","o")
+
+write.csv(salida_test1, file = "./output/data/approach1/test1/test1_nb.ttl", fileEncoding = "UTF-8", row.names=FALSE)
+
+
+##############
+#RandomForest#
+##############
+
+
+rf_pruebaHojas_test1 <- h2o.randomForest(
+  model_id="rf_pruebaHojas_test1",
+  training_frame=train_test1, 
+  validation_frame=valid_test1[,2:ncol(valid_test1)],
+  x=2:(ncol(train_test1)-1),
+  y=ncol(train_test1),
+  ntrees = 200,
+  max_depth = 120,
+  stopping_rounds = 3,
+  score_each_iteration = T,
+  seed = 1234)
+h2o.saveModel(rf_pruebaHojas_test1, path="./data/approach1/test1/")
+
+test1 <- h2o.predict(object = rf_pruebaHojas_test1, newdata = valid_test1[,2:(ncol(valid_test1)-1)])[1]
+
+salida_test1 <- cbind(validating_test1[,1],as.data.frame(test1))
+colnames(salida_test1) <- c("s","o")
+write.csv(salida_test1, file = "./output/data/approach1/test1/test1_rf.ttl", fileEncoding = "UTF-8", row.names=FALSE)
+
+
+
+dl_pruebaHojas_test1 <- h2o.deeplearning(
+  model_id="dl_pruebaHojas_test1",
+  training_frame=train_test1, 
+  validation_frame=valid_test1[,2:ncol(valid_test1)],
+  x=2:(ncol(train_test1)-1),
+  y=ncol(train_test1),
+  stopping_rounds = 0,
+  seed = 1234)
+h2o.saveModel(dl_pruebaHojas_test1, path="./data/approach1/test1")
+
+test1 <- h2o.predict(object = dl_pruebaHojas_test1, newdata = valid_test1[,2:(ncol(valid_test1)-1)])[1]
+
+
+salida_test1 <- cbind(validating_test1[,1],as.data.frame(test1))
+colnames(salida_test1) <- c("s","o")
+
+write.csv(salida_test1, file = "./output/data/approach1/test1/test1_dl.ttl", fileEncoding = "UTF-8", row.names=FALSE)
+
+
+
+
+
+
+
+
+
